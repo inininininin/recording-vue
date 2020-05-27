dac7d9c6f4775b049be19316e728755c64dcb202
onerunsall <413038044@qq.com>
2020-05-27 11:02:28 +0800

    1

---
+++ src/components/register.vue
@@ -0,0 +1,71 @@
+<template>
+    <div  id="register" >
+		<div style="height:30px;"></div>
+		<h1 style="text-align: center;font-size: 24px;margin:0;">Recording</h1>
+
+		<div style="height:30px;"></div>
+
+		<form style="width:250px;margin:auto;">
+			<div style="height:30px;line-height:30px;position:relative;">
+				<span style="width:50px;display: inline-block;font-size: 14px;">手机</span>
+				<input v-model="phone" type="text" style="width:183px;height:28px;border-width: 1px;padding:0;padding-right:15px;" />
+				<span v-if="phone" style="font-size: 14px;position:absolute;right:6px;cursor: pointer;color: #8f8f8f;" @click="phone=null">x</span>
+			</div>
+
+			<div style="height:10px;"></div>
+			<div style="height:30px;line-height:30px;position:relative;" >
+				<span style="width:50px;display: inline-block;font-size: 14px;">密码</span>
+				<input v-model="password" type="password" style="width:183px;height:28px;border-width: 1px;padding:0;padding-right:15px;"/>
+				<span v-if="password" style="font-size: 14px;position:absolute;right:6px;cursor: pointer;color: #8f8f8f;" @click="password=null">x</span>
+			</div>
+
+			<div style="height:10px;" ></div>
+			<div style="height:30px;line-height:30px;position:relative;">
+				<span style="width:50px;display: inline-block; font-size: 14px;">验证码</span>
+				<input v-model="smsvcode" type="text" style="width:138px;height:28px;border-width: 1px;padding:0;padding-right:15px;"/>
+				<span v-if="smsvcode" style="font-size: 14px;position:absolute;right:50px;cursor: pointer;color: #8f8f8f;" @click="smsvcode=null">x</span>
+				<span style="width:10px;display:inline-block;"></span>
+				<button style="width:35px;height:30px;padding:0;vertical-align: top;">获取</button>
+			</div>
+
+			<div style="height:50px;"></div>
+			<div style="text-align: center;">
+				<button style="width:200px;height:30px;" @click="">注册</button>
+				<div style="height:50px;"></div>
+				<button style="width:200px;height:30px;" @click="$router.push({path:'/login',query:{time:new Date().getTime()}})">去登录</button>
+			</div>
+		</form>
+    </div>
+</template>     
+<script>
+
+export default {
+	name: 'register',
+	data () {
+		return {
+			phone:null,
+			password:null,
+			smsvcode:null,
+		}
+    },
+	activated(){
+       debugger
+        let thisVue = this;
+		if(thisVue.query != JSON.stringify(thisVue.$route.query)){
+			Object.assign(thisVue.$data, thisVue.$options.data());
+			thisVue.query = JSON.stringify(thisVue.$route.query);
+			thisVue.reload();
+
+		}
+	},
+	methods:{
+		reload(){
+
+         }
+	}		
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/task.vue
@@ -0,0 +1,57 @@
+<template>
+	<div id="index" class="scrollbar" style="height:100%;overflow:auto;">
+		<div
+			style="font-size: 16px;text-align: center;height:40px;line-height: 40px;position: relative;border-bottom:1px solid #8F8F8f">
+			<span @click="$router.back()" style="position: absolute;left:0;width:40px;cursor: pointer;">&lt;</span>
+			<span>任务详情</span>
+		</div>
+		<div style="padding:0 10px;margin-top:20px;">
+			<div style="font-size: 16px;">名称</div>
+			<div><input type="text" style="width:100%;margin:0;padding:3px;border:1px solid #8f8f8f;" /></div>
+
+			<div style="font-size: 16px;">图片</div>
+			<div><input type="text" style="width:100%;margin:0;padding:3px;border:1px solid #8f8f8f;" /></div>
+
+			<div style="font-size: 16px;">内容</div>
+			<div><input type="text" style="width:100%;margin:0;padding:3px;border:1px solid #8f8f8f;" /></div>
+
+			<div style="text-align: center;margin-top:30px;">
+				<button @click="newTask()" style="width:250px;height:35px;">发布</button>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'index',
+		data() {
+			return {
+				query: '',
+			}
+		},
+		activated() {
+			let thisVue = this
+
+			if (thisVue.query != JSON.stringify(thisVue.$route.query)) {
+				Object.assign(thisVue.$data, thisVue.$options.data());
+				thisVue.query = JSON.stringify(thisVue.$route.query);
+				thisVue.reload();
+
+			}
+		},
+		methods: {
+			reload() {
+
+			},
+			newTask() {
+				let thisVue = this
+				thisVue.$router.back()
+			},
+		}
+	}
+</script>
+<style scoped>
+	.scrollbar::-webkit-scrollbar {
+		display: none;
+	}
+</style>
